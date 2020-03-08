f760e17b974924d42741f014446a9191d47ddb53
Ted Barrett <21516680+BarneyKB@users.noreply.github.com>
2020-03-08 23:05:44 +1100

    added comments and formatted

--- subnamer.py
+++ subnamer.py
@@ -1,13 +1,18 @@
 import os
 import re
 
+# gets list of all files in the directory in which the program is being run
 files = [f for f in os.listdir()]
 
+# creates list of subtitle files (anything ending in "srt"), and also finds the episode and series number. Item format: ["filename", [int(seriesNumber), int(episodeNumber)]
+subs = [(f, [int(i) for i in re.findall(r'\d+', re.findall(r's\d{1,2}e\d{1,2}', f.lower())[0])]) for f in files if f[-3:].lower() == "srt"]
 
-subs = [(f,[int(i) for i in re.findall(r'\d+',re.findall(r's\d{1,2}e\d{1,2}',f.lower())[0])]) for f in files if f[-3:].lower() == "srt"]
-videos = [(f,[int(i) for i in re.findall(r'\d+',re.findall(r's\d{1,2}e\d{1,2}',f.lower())[0])]) for f in files if f[-3:].lower() in ["mkv","mov","mp4"]]
+# creates list of video files (anything ending in "mkv", "mov" or "mp4"), and also finds the episode and series number. Item format: ["filename", [int(seriesNumber), int(episodeNumber)]
+videos = [(f, [int(i) for i in re.findall(r'\d+', re.findall(r's\d{1,2}e\d{1,2}', f.lower())[0])]) for f in files if f[-3:].lower() in ["mkv", "mov", "mp4"]]
 
+# loops through all subtitle files
 for subName, subSE in subs:
     for vidName, vidSE in videos:
+        # if there is a video file with matching series and episode, rename the subtitle file to have the same name (except extension)
         if subSE == vidSE:
-            os.rename(subName,str(vidName[:-3]+"srt"))
+            os.rename(subName, str(vidName[:-3] + "srt"))
